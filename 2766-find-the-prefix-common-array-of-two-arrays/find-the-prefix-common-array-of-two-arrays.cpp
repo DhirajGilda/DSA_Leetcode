b60690f5689c9a6ad894d9720758ec1bb9558e9c
Dhiraj <80347604+DhirajGilda@users.noreply.github.com>
2025-01-14 20:56:30 +0530

    Time: 8 ms (53.56%) | Memory: 88.2 MB (47.73%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        unordered_map<int,int>mp;
-        int n=A.size();
-        vector<int>ans;
-        if(A[0]==B[0]){
-            ans.push_back(1);
-        }
-        else{
-            ans.push_back(0);
-        }
-        mp[A[0]]++;
-        mp[B[0]]++;
-        for(int i=1;i<n;i++ ){
-            int push=ans[i-1];
-            if(A[i]==B[i]){
-                push+=1;
-                ans.push_back(push);
-            }else{
-                push+=mp[A[i]];
-                push+=mp[B[i]];
-                ans.push_back(push);
+        int n = A.size();
+        unordered_set<int> seen; // To track the elements we've encountered
+        vector<int> ans(n);     // Result array
+        int commonCount = 0;    // To count common elements
+
+        for (int i = 0; i < n; i++) {
+            // Check for A[i] in seen set
+            if (seen.count(A[i])) {
+                commonCount++;
+            } else {
+                seen.insert(A[i]);
             }
-            mp[A[i]]++;
-        mp[B[i]]++;
+
+            // Check for B[i] in seen set
+            if (seen.count(B[i])) {
+                commonCount++;
+            } else {
+                seen.insert(B[i]);
+            }
+
+            // Update the result for the current prefix
+            ans[i] = commonCount;
         }
 
         return ans;
