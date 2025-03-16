8a5ddcfa4c12fe20ac64e8396cc36c61a31387d8
Dhiraj <80347604+DhirajGilda@users.noreply.github.com>
2025-03-16 18:44:56 +0530

    Time: 1 ms (57.14%) | Memory: 29.6 MB (0.00%) - LeetSync

--- 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.cpp
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     int maxSum(vector<int>& nums) {
-    sort(nums.begin(),nums.end());
-    if(nums[nums.size()-1]<=0){
-        return nums[nums.size()-1];
-    }
+    // sort(nums.begin(),nums.end());
+    // if(nums[nums.size()-1]<=0){
+    //     return nums[nums.size()-1];
+    // }
     unordered_map<int,int>mp;
     int ans=0;
+    int negAns=INT_MIN;
     for(auto i:nums){
         mp[i]++;
     }
 
     for(auto i:mp){
-        if(i.first>=0){
+        if(i.first>0){
             ans+=i.first;
         }
+        else{
+            if(i.first>negAns){
+                negAns=i.first;
+            }
+        }
     }
-    return ans;
+    cout<<ans;
+    if(ans){
+        return ans;
+    }
+    return negAns;
     }
 };
