ea65c93eecce898b989316935e47b806b5911efb
Dhiraj <80347604+DhirajGilda@users.noreply.github.com>
2025-11-04 23:18:43 +0530

    Time: 33 ms (7.35%) | Memory: 37.2 MB (25.56%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -1,6 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     vector<int> findXSum(vector<int>& nums, int k, int x) {
@@ -10,13 +7,9 @@ public:
 
         for (int i = 0; i < outputArraySize; i++) {
             unordered_map<int, int> freq;
-            
-            // Build frequency map for subarray nums[i...i+k-1]
             for (int j = i; j < i + k; j++) {
                 freq[nums[j]]++;
             }
-
-            // Priority queue sorted by frequency (desc), then value (desc)
             priority_queue<pair<int,int>> pq; 
             for (auto &p : freq) {
                 pq.push({p.second, p.first});
